cea8080186e01082075252755458379747d0e0f3
JesseTucker <jesseDtucker@gmail.com>
2014-10-20 02:11:40 -0600

    Added shell scripts

---
+++ run_timeout.sh
@@ -0,0 +1,2 @@
+java -classpath ./bin/production/ECE422-Assignment1/ com.jetucker.Generator unsorted.txt 100000000
+java -classpath ./bin/production/ECE422-Assignment1/ com.jetucker.Driver unsorted.txt out.txt 0.0 0.0 5000
